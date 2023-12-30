9bab297bdb70de96434cb2976dc51e4efdb065d7
Vimanshu Aryan <vimanshuaryan@gmail.com>
2023-12-30 11:57:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((response) => response.json())
+  //     .then((users) => setMonsters(users));
+  // }, []);
+
+  useEffect(() => {
+    const pokemon = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json();
+    setMonsters(users);
+  }
+    pokemon()}, []);
+
+  const onChangeHandler = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  useEffect (() => {
+    const newMonsters = monsters.filter((monster) => 
+    return monster.name.toLocaleLowerCase().includes(searchField);
+    );
+    
+    setMonsters(newMonsters);
+  },[searchField]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Monsters Rolodex</h1>
+      <input onChange={onChangeHandler}/>
+      {console.log(monsters)}
+      <div>
+        {monsters.map((monster) =>
+          <div key={monster.id}>{monster.name}</div>)}
+      </div>
+      {/* <CardList/> */}
     </div>
   );
 }
